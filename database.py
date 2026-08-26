1e5943b0210bd623db8d42c594c77f95aa999377
Mangal9101 <mkp94065@gmail.com>
2026-08-26 08:21:23 +0530

    Update database.py

--- database.py
+++ database.py
@@ -1,12 +1,7 @@
-import os
-
 from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base, sessionmaker
 
-if os.environ.get("RENDER"):
-    DATABASE_URL = "sqlite:////data/sawariya.db"
-else:
-    DATABASE_URL = "sqlite:///./sawariya.db"
+DATABASE_URL = "sqlite:///./sawariya.db"
 
 engine = create_engine(
     DATABASE_URL,
