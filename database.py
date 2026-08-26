b30569fffd02b4a26e2a5142260f9455d40d4c4f
Mangal9101 <mkp94065@gmail.com>
2026-08-26 08:18:42 +0530

    Update database.py

--- database.py
+++ database.py
@@ -1,10 +1,26 @@
+import os
+
 from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base, sessionmaker
 
-DATABASE_URL = "sqlite:///./sawariya.db"
-engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
-SessionLocal = sessionmaker(bind=engine, autocommit=False, autoflush=False)
+if os.environ.get("RENDER"):
+    DATABASE_URL = "sqlite:////data/sawariya.db"
+else:
+    DATABASE_URL = "sqlite:///./sawariya.db"
+
+engine = create_engine(
+    DATABASE_URL,
+    connect_args={"check_same_thread": False}
+)
+
+SessionLocal = sessionmaker(
+    bind=engine,
+    autocommit=False,
+    autoflush=False
+)
+
 Base = declarative_base()
+
 
 def get_db():
     db = SessionLocal()
